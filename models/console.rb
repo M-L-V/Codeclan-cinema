26018bd3aaf9059340c356e6b991f2efac422a94
Morgaine <morgainedv@gmail.com>
2019-11-03 18:07:58 +0000

    add instances

--- models/console.rb
+++ models/console.rb
@@ -8,10 +8,36 @@ Ticket.delete_all()
 Film.delete_all()
 Customer.delete_all()
 #
-# movie1 = Movie.new({
-#     'title' => 'La La Land',
-#     'genre' => 'musical',
-#     'budget' => 30000000
-#   })
-#
-#   movie1.save()
+
+film1 = Film.new({
+  'title' => 'Into the Spider-Verse',
+    'price' => 10
+  })
+
+# film1.save
+
+film2 = Film.new({
+  'title' => 'Homecoming'
+  'price' => 9
+  })
+
+# film2.save
+
+customer1 = Customer.new({
+  'name' => 'Peter'
+  'funds' => 20
+  })
+
+# customer1.save
+
+customer2 = Customer.new ({
+  'name' => 'Miles'
+  'funds' => 15
+  })
+
+  # customer2.save
+
+  # ticke1 = Ticket.new ({
+  #   'customer_id' => customer.id,
+  #   'film_id' => film.id
+  #   })
