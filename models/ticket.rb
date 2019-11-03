5e837cabb07c24d80b438fcec8e983e15853eab3
Morgaine <morgainedv@gmail.com>
2019-11-03 19:15:18 +0000

    ticket methods

--- models/ticket.rb
+++ models/ticket.rb
@@ -11,6 +11,43 @@ class Ticket
   end
 
 
+  def save()
+      sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id"
+      values = [@customer_id, @film_id]
+      film = SqlRunner.run(sql, values).first
+      @id = ticket['id'].to_i
+    end
+
+
+  def update()
+      sql = "UPDATE customers SET (customer_id, film_id) = ($1, $2) WHERE id = $3"
+      values = [@customer_id, @film_id]
+      SqlRunner.run(sql, values)
+  end
+
+
+
+
+
+
+
+
+
+
+
+  def save()
+      sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id"
+      values = [@name, @funds]
+      film = SqlRunner.run(sql, values).first
+      @id = film['id'].to_i
+    end
+
+
+  def update()
+      sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+      values = [@name, @funds, @id]
+      SqlRunner.run(sql, values)
+  end
 #save, update, and delete methods below
 
 
